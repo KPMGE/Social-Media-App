4c8b8c2f189a1c901f1d1dfec73658b989f3e4be
KPMGE <jesuskevin254@gmail.com>
2021-06-01 09:56:11 -0300

    Adding authentication page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
+import Auth from "./user/pages/Auth";
 
 const App = () => {
   return (
@@ -33,6 +34,10 @@ const App = () => {
 
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+
+          <Route path="/auth">
+            <Auth />
           </Route>
 
           <Redirect to="/" />
