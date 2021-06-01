77eb12589c37dc8a101512aa926b7d11d81b4e85
KPMGE <jesuskevin254@gmail.com>
2021-06-01 08:47:50 -0300

    Adding suport to initial values for input component

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -25,16 +25,16 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: "",
+    value: props.initialValue || "",
     isTouched: false,
-    isValid: false,
+    isValid: props.initialValid || false,
   });
 
-  const {id, onInput} = props;
-  const {value, isValid} = inputState;
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
 
   useEffect(() => {
-    onInput(id, value, isValid)
+    onInput(id, value, isValid);
   }, [id, value, isValid, onInput]);
 
   const changeHandler = (event) => {
