c93e9c90f9cc2a0fc544aa95e6d126e890b3b74c
KPMGE <jesuskevin254@gmail.com>
2021-06-02 11:58:47 -0300

    Adding suport to update and delete operations and fixing getPlacesByUserId bug

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,7 +1,7 @@
 import { HttpError } from "../models/http-error.js";
 import { v4 as generateUniqueId } from "uuid";
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire state building",
@@ -31,20 +31,20 @@ export const getPlaceById = (req, res, next) => {
   res.json({ place }); // place: place
 };
 
-export const getPlaceByUserId = (req, res, next) => {
+export const getPlacesByUserId = (req, res, next) => {
   const { userId } = req.params;
 
-  const place = DUMMY_PLACES.find((place) => {
+  const places = DUMMY_PLACES.find((place) => {
     return place.creatorId === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find a place for the provided user id", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 export const createPlace = (req, res, next) => {
@@ -61,4 +61,27 @@ export const createPlace = (req, res, next) => {
 
   DUMMY_PLACES.push(newPlace);
   res.status(201).json({ place: newPlace });
+};
+
+export const updatePlace = (req, res, next) => {
+  const { placeId } = req.params;
+  const { title, description } = req.body;
+
+  const updatedPlace = {
+    ...DUMMY_PLACES.find((place) => place.id === placeId),
+  };
+  const placeIndex = DUMMY_PLACES.findIndex((place) => place.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+export const deletePlace = (req, res, next) => {
+  const { placeId } = req.params;
+  DUMMY_PLACES = DUMMY_PLACES.filter((place) => place.id !== placeId);
+
+  res.status(200).json({ message: "Deleted place." });
 };
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,14 +1,18 @@
 import express from "express";
 import {
   getPlaceById,
-  getPlaceByUserId,
+  getPlacesByUserId,
   createPlace,
+  updatePlace,
+  deletePlace,
 } from "../controllers/places-controllers.js";
 
 const router = express.Router();
 
 router.get("/:placeId", getPlaceById);
-router.get("/user/:userId", getPlaceByUserId);
+router.get("/user/:userId", getPlacesByUserId);
 router.post("/", createPlace);
+router.patch("/:placeId", updatePlace);
+router.delete("/:placeId", deletePlace);
 
 export default router;
