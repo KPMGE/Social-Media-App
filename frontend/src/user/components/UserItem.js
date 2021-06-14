a8d33f0d43a21671e094054abad6fb47ad4f5802
KPMGE <jesuskevin254@gmail.com>
2021-06-14 15:04:00 -0300

    Adjusting file upload

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -12,7 +12,10 @@ const UserItem = (props) => {
       <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
-            <Avatar image={props.image} alt={props.name} />
+            <Avatar
+              image={`http://localhost:5000/${props.image}`}
+              alt={props.name}
+            />
           </div>
 
           <div className="user-item__info">
