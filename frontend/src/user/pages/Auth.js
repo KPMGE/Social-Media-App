8f8428a10efcd9a52252c01e0686564089953500
KPMGE <jesuskevin254@gmail.com>
2021-06-14 08:40:08 -0300

    Implementing file upload on frontend

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -118,6 +119,10 @@ const Auth = () => {
               errorText="Please Enter your name."
               onInput={inputHandler}
             />
+          )}
+
+          {!isLoginMode && (
+            <ImageUpload center id="image" onInput={inputHandler} />
           )}
 
           <Input
