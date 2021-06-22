5b5a068fa55f6c6feef4701ace2f3c05498b9333
KPMGE <jesuskevin254@gmail.com>
2021-06-22 10:01:00 -0300

    adding environmental variables on frontend

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -42,7 +42,7 @@ const UpdatePlace = () => {
     const fetchPlace = async () => {
       try {
         const responseData = await sendRequest(
-          `http://localhost:5000/api/places/${placeId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`
         );
 
         setLoadedPlace(responseData.place);
@@ -70,7 +70,7 @@ const UpdatePlace = () => {
 
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${placeId}`,
+        `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
         "PATCH",
         JSON.stringify({
           title: formState.inputs.title.value,
